16f1adb7031fd8275578509f969716e9865c69d2
Dallas Kinzel <dkinzel@gmail.com>
2013-02-16 13:58:05 -0700

    remove mvc structure, added some example game code

--- www/controllers/main.js
+++
@@ -1,8 +0,0 @@
-function onDeviceReady(){
-    /*$('#button').bind('tap', methodName);
-    $('#storageButton').bind('tap',function(){alert('implement me!')});*/
-}
-
-function init(){
-    document.addEventListener("deviceready", onDeviceReady,false);
-}
