8c83266cb3d834e4930ed0e37871442a9de58903
Dallas Kinzel <mintassin@hotmail.com>
2013-02-18 21:51:31 -0700

    added createjs, added second page to implement createjs but didn't get it done

---
+++ www/index2.js
@@ -0,0 +1,119 @@
+﻿var game;
+
+$(document).ready(function () {
+    game = {
+        fps: 30,
+        Width: window.innerWidth,
+        Height : window.innerHeight,
+        Context : $("#gameCanvas")[0].getContext("2d"),
+        Sources : {
+            beach: 'beach.png',
+            snake: 'snake.png',
+            snake_glow: 'snake-glow.png',
+            snake_black: 'snake-black.png',
+            lion: 'lion.png',
+            lion_glow: 'lion-glow.png',
+            lion_black: 'lion-black.png',
+            monkey: 'monkey.png',
+            monkey_glow: 'monkey-glow.png',
+            monkey_black: 'monkey-black.png',
+            giraffe: 'giraffe.png',
+            giraffe_glow: 'giraffe-glow.png',
+            giraffe_black: 'giraffe-black.png',
+        },
+        Images: null,
+        ImagesFolder: 'resources/images/' 
+    };
+
+
+    resize(game.Width,game.Height);
+
+    loadImages(game, function (images) {
+                                        game.Images = images;
+                                        setInterval(function () {
+                                            update();
+                                            draw(game.Context, game.Width, game.Height);
+                                        }, 1000 / game.fps);
+    })
+});
+
+$(window).resize(function () {
+    game.Width = window.innerWidth;
+    game.Height = window.innerHeight;
+    resize(game.Width, game.Height);
+});
+
+function resize(width, height) {
+    $("#gameArea").width = width;
+    $("#gameArea").height = height;
+
+    $("#gameArea").prop({
+        width: width,
+        height: height
+    });
+
+    $("#gameCanvas")[0].width = width;
+    $("#gameCanvas")[0].height = height;
+}
+
+function loadImages(game, callback) {
+    var images = {};
+    var loadedImages = 0;
+    var numImages = 0;
+    for (var src in game.Sources) {
+        numImages++;
+    }
+    for (var src in game.Sources) {
+        images[src] = new Image();
+        images[src].onload = function () {
+            if (++loadedImages >= numImages) {
+                callback(images);
+            }
+        };
+        images[src].src = game.ImagesFolder + game.Sources[src];
+    }
+}
+
+function update() {
+    text.update();
+}
+
+function draw(context,width,height) {
+    //context.clearRect(0, 0, width, height);
+    
+    background.draw(game.Images.beach,context, width, height);
+    text.draw(context);
+    square.draw(context);
+    
+}
+
+var background = {
+    draw: function (image,context,width,height) {
+        //context.fillStyle = "#000";
+        //context.fillRect(5, 5, width - 10, height - 10);
+        context.drawImage(image, 0, 0,width,height);
+    }
+}
+
+var text = {
+    textX : 50,
+    textY : 50,
+    
+    draw: function (context) {
+        context.fillStyle = "green";
+        context.fillText("Sup Bro!", this.textX, this.textY);
+    },
+
+    update: function () {
+        this.textX += 1;
+        this.textY += 1;
+    }
+}
+
+var square = {
+    draw: function (context) {
+        context.fillStyle = 'red';
+        context.fillRect(30, 30, 50, 50);
+    }
+}
+
