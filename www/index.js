c8d8477f35af6f1b387480ebd1626600c38ad6f1
DalKinzel <dkinzel@gmail.com>
2013-02-19 14:09:05 -0700

    refactor for easeljs

--- www/index.js
+++ www/index.js
@@ -1,40 +1,17 @@
-﻿var game;
-
-$(document).ready(function () {
-    game = {
-        InitialWidth: 578,
-        InitialHeight: 530,
-        Width: window.innerWidth,
-        Height: window.innerHeight,
-        ScaleFactorX: null,
-        ScaleFactorY: null,
-        Context: $("#gameCanvas")[0].getContext("2d"),
-        Stage: new createjs.Stage($("#gameCanvas")[0]),
-        ImagesFolder: 'resources/images/',
-        Manifest: null,
-        Queue: new createjs.LoadQueue(true)
-    };
-
-    game.Manifest =[{id:"beach",src : game.ImagesFolder + "beach.png"},
-                    {id:"snake",src : game.ImagesFolder + "snake.png"},
-                    {id:"snake_glow",src : game.ImagesFolder + "snake-glow.png"},
-                    {id:"snake_black",src : game.ImagesFolder + "snake-black.png"},
-                    {id:"lion",src : game.ImagesFolder + "lion.png"},
-                    {id:"lion_glow",src : game.ImagesFolder + "lion-glow.png"},
-                    {id:"lion_black",src : game.ImagesFolder + "lion-black.png"},
-                    {id:"monkey",src : game.ImagesFolder + "monkey.png"},
-                    {id:"monkey_glow",src : game.ImagesFolder + "monkey-glow.png"},
-                    {id:"monkey_black",src : game.ImagesFolder + "monkey-black.png"},
-                    {id:"giraffe",src : game.ImagesFolder + "giraffe.png"},
-                    {id:"giraffe_glow",src : game.ImagesFolder + "giraffe-glow.png"},
-                    {id:"giraffe_black", src: game.ImagesFolder + "giraffe-black.png" }]
-
-
-    resize(game.Width,game.Height);
-
-    game.Queue.addEventListener("complete", prepareStage);
-    game.Queue.loadManifest(game.Manifest);
-});
+﻿var game = {
+    InitialWidth: 578,
+    InitialHeight: 530,
+    Width: window.innerWidth,
+    Height: window.innerHeight,
+    ScaleFactorX: null,
+    ScaleFactorY: null,
+    Context: null,
+    Stage: null,
+    ImagesFolder: 'resources/images/',
+    Manifest: null,
+    Queue: new createjs.LoadQueue(true),
+    ShouldDraw: true
+}
 
 $(window).resize(function () {
     game.Width = window.innerWidth;
@@ -43,6 +20,29 @@ $(window).resize(function () {
     game.ScaleFactorY = game.Height / game.InitialHeight;
     resize(game.Width, game.Height);
 });
+
+$(document).ready(function () {
+    game.Context = $("#gameCanvas")[0].getContext("2d");
+    game.Stage = new createjs.Stage($("#gameCanvas")[0]);
+    //game.Stage.autoClear = false;
+    game.Manifest = [{ id: "beach", src: game.ImagesFolder + "beach.png" },
+                    { id: "snake", src: game.ImagesFolder + "snake.png" },
+                    { id: "snake_glow", src: game.ImagesFolder + "snake-glow.png" },
+                    { id: "snake_black", src: game.ImagesFolder + "snake-black.png" },
+                    { id: "lion", src: game.ImagesFolder + "lion.png" },
+                    { id: "lion_glow", src: game.ImagesFolder + "lion-glow.png" },
+                    { id: "lion_black", src: game.ImagesFolder + "lion-black.png" },
+                    { id: "monkey", src: game.ImagesFolder + "monkey.png" },
+                    { id: "monkey_glow", src: game.ImagesFolder + "monkey-glow.png" },
+                    { id: "monkey_black", src: game.ImagesFolder + "monkey-black.png" },
+                    { id: "giraffe", src: game.ImagesFolder + "giraffe.png" },
+                    { id: "giraffe_glow", src: game.ImagesFolder + "giraffe-glow.png" },
+                    { id: "giraffe_black", src: game.ImagesFolder + "giraffe-black.png" }];
+
+    game.Queue.addEventListener("complete", prepareStage);
+    game.Queue.loadManifest(game.Manifest);
+});
+
 
 function resize(width, height) {
     $("#gameArea").width = width;
@@ -55,15 +55,15 @@ function resize(width, height) {
 
     $("#gameCanvas")[0].width = width;
     $("#gameCanvas")[0].height = height;
+
+    game.shouldDraw = true;
 }
 
 function prepareStage() {
-    background.draw(game.Queue.getResult("beach"));
-    /*setInterval(function () {
-        update();
-        draw(game, game.Width, game.Height);
-    }, 1000 / game.fps);}
-    */
+    resize(game.Width, game.Height);
+
+    background.bmp = new createjs.Bitmap(game.Queue.getResult("beach"));
+    background.draw();
 
     startGame();
 }
@@ -75,49 +75,29 @@ function startGame() {
 
 function tick(event) {
     //update();
-    draw(game, game.Width, game.Height);
+   // if (game.ShouldDraw == true) {
+        draw(game);
+        game.ShouldDraw = false;
+    //}
 }
 
 function update() {
     text.update();
 }
 
-function draw(game,width,height) {
-    background.draw(game.Queue.getResult("beach"));
+function draw(game) {
+    background.draw();
     //text.draw(game.Context);
     //square.draw(game.Context);
 }
 
 var background = {
-    draw: function (image, context, width, height) {
-        bmp = new createjs.Bitmap(image)
-        bmp.scaleX = game.ScaleFactorX;
-        bmp.scaleY = game.ScaleFactorY;
-        game.Stage.addChild(bmp);
+    bmp: null,
+    draw: function () {
+        this.bmp.scaleX = game.ScaleFactorX;
+        this.bmp.scaleY = game.ScaleFactorY;
+        game.Stage.addChild(this.bmp);
         game.Stage.update();
     }
 }
-
-var text = {
-    textX : 50,
-    textY : 50,
-    
-    draw: function (context) {
-        context.fillStyle = "green";
-        context.fillText("Sup Bro!", this.textX, this.textY);
-    },
-
-    update: function () {
-        this.textX += 1;
-        this.textY += 1;
-    }
-}
-
-var square = {
-    draw: function (context) {
-        context.fillStyle = 'red';
-        context.fillRect(30, 30, 50, 50);
-    }
-}
-
 
